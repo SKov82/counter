33a656f3f321ea6678ae9ce3aeb5e8d88fb836da
skov82 <skov82@ya.ru>
2022-05-20 14:06:57 +0300

    refactor reducer

--- src/redux/counter-reducer.ts
+++ src/redux/counter-reducer.ts
@@ -8,6 +8,11 @@ export enum ACTIONS_TYPE {
     SET_MAX_COUNT = 'SET_MAX_COUNT',
 }
 
+export type ActionType = {
+    type: ACTIONS_TYPE
+    value?: number
+}
+
 export type StateType = {
     count: number
     minCount: number
@@ -31,46 +36,26 @@ export const counterReducer = (state: StateType = initialState, action: ActionTy
         case ACTIONS_TYPE.SWITCH_MODE:
             return {...state, isCounterMode: !state.isCounterMode, count: state.minCount}
         case ACTIONS_TYPE.SET_MIN_COUNT:
-            return {
+            return (action.value !== undefined) ? {
                 ...state,
-                // @ts-ignore
-                minCount: action.minCount,
-                // @ts-ignore
-                maxCount: (action.minCount >= state.maxCount)
-                    // @ts-ignore
-                    ? action.minCount + 1
-                    : state.maxCount
-            }
+                minCount: action.value,
+                maxCount: (action.value >= state.maxCount) ? action.value + 1 : state.maxCount
+            } : state
         case ACTIONS_TYPE.SET_MAX_COUNT:
-            return {
+            return (action.value) ? {
                 ...state,
                 minCount: state.minCount,
-                // @ts-ignore
-                maxCount: (action.maxCount > state.minCount)
-                    // @ts-ignore
-                    ? action.maxCount
-                    : state.maxCount
-            }
+                maxCount: (action.value > state.minCount) ? action.value : state.maxCount
+            } : state
         default:
             return state
     }
 }
 
-type IncreaseCountACType = ReturnType<typeof increaseCountAC>
-export const increaseCountAC = () => ({type: ACTIONS_TYPE.INCREASE_COUNT})
-
-type ResetCountACType = ReturnType<typeof resetCountAC>
-export const resetCountAC = () => ({type: ACTIONS_TYPE.RESET_COUNT})
-
-type changeModeACType = ReturnType<typeof changeModeAC>
-export const changeModeAC = () => ({type: ACTIONS_TYPE.SWITCH_MODE})
-
-type setMinCountACType = ReturnType<typeof setMinCountAC>
-export const setMinCountAC = (value: number) => ({type: ACTIONS_TYPE.SET_MIN_COUNT, minCount: value})
-
-type setMaxCountACType = ReturnType<typeof setMaxCountAC>
-export const setMaxCountAC = (value: number) => ({type: ACTIONS_TYPE.SET_MAX_COUNT, maxCount: value})
-
-export type ActionType = IncreaseCountACType | ResetCountACType | changeModeACType | setMinCountACType | setMaxCountACType
+export const increaseCountAC = (): ActionType => ({type: ACTIONS_TYPE.INCREASE_COUNT})
+export const resetCountAC = (): ActionType => ({type: ACTIONS_TYPE.RESET_COUNT})
+export const changeModeAC = (): ActionType => ({type: ACTIONS_TYPE.SWITCH_MODE})
+export const setMinCountAC = (value: number): ActionType => ({type: ACTIONS_TYPE.SET_MIN_COUNT, value: value})
+export const setMaxCountAC = (value: number): ActionType => ({type: ACTIONS_TYPE.SET_MAX_COUNT, value: value})
 
 export const selectAll = (store: AppStateType) => store.counter
